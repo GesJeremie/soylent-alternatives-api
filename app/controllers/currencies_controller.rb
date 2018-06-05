7051e12a3cd25ea8e1bb90c4e8a7a077239e1fca
Jeremie Ges <jges@weblinc.com>
2018-06-04 19:53:14 -0700

    Change endpoint api.fixer.io

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,4 +1,7 @@
 class CurrenciesController < ApplicationController
+  
+  FIXER_API_KEY = '5d7a931c37d2c0eb5c91dbcca63aeaeb'.freeze
+
   def show
     base = params[:base] || 'EUR'
     render json: get_response(base)
@@ -7,7 +10,7 @@ class CurrenciesController < ApplicationController
   private
     def get_response(base)
       if Currency.where(base: base).empty?
-        response = HTTParty.get("http://api.fixer.io/latest?base=#{base}").body
+        response = HTTParty.get("http://data.fixer.io/api/latest?base=#{base}&access_key=#{access_key}").body
 
         currency = Currency.new do |currency|
           currency.base = base
@@ -20,7 +23,7 @@ class CurrenciesController < ApplicationController
       end
 
       if Currency.where(base: base).first.updated_at < 1.day.ago 
-        response = HTTParty.get("http://api.fixer.io/latest?base=#{base}").body
+        response = HTTParty.get("http://data.fixer.io/api/latest?base=#{base}&access_key=#{access_key}").body
 
         currency = Currency.where(base: base).first
         currency.response = response
@@ -30,5 +33,9 @@ class CurrenciesController < ApplicationController
       end
 
       return Currency.where(base: base).first.response
+    end
+
+    def access_key
+      FIXER_API_KEY
     end
 end
